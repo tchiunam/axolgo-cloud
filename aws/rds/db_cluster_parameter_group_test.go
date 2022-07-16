373f0d2564d79d2dbb4b07958a9da80f9c05aa81
tchiunam <tchiunam@users.noreply.github.com>
2022-07-16 23:40:45 +0800

    Add aws.rds automated test

---
+++ aws/rds/db_cluster_parameter_group_test.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2022 tchiunam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package rds
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/tchiunam/axolgo-lib/types"
+)
+
+// TestRunModifyDBClusterParameterGroupInvaoid calls RunModifyDBClusterParameterGroup
+// and expects error since the AWS credentials are not set.
+func TestRunModifyDBClusterParameterGroupInvalid(t *testing.T) {
+	paramName := "some-parameter-name"
+	paramValue := "some-parameter-value"
+
+	cases := map[string]struct {
+		parameterGroupName  string
+		staticParameters    []types.Parameter
+		dynamicParameters   []types.Parameter
+		expectStringInError string
+	}{
+		"static params": {
+			parameterGroupName: "dev-db-parameter-group",
+			staticParameters: []types.Parameter{{
+				Name:  &paramName,
+				Value: &paramValue,
+			}},
+			expectStringInError: "failed to retrieve credentials",
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			_, err := RunModifyDBClusterParameterGroup(c.parameterGroupName, c.staticParameters, c.dynamicParameters)
+			if !strings.Contains(err.Error(), c.expectStringInError) {
+				t.Errorf("Expected error containing %q, got %s", c.expectStringInError, err)
+			}
+		})
+	}
+}
